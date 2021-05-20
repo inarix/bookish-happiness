7d93b5c36d05167131ac1ca8bae8c98ea61ebb0c
saisona <alexandre.saison.pro@gmail.com>
2021-05-21 00:46:43 +0200

    Update: adding new cat data.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -92,95 +92,49 @@ function checkEnvVariables() {
 
 function generateApplicationSpec() {
 echo "Generating ApplicationSpec"
-cat >./data.json <<EOF 
-{
-    "metadata": {
-        "name": "$APPLICATION_NAME",
-        "namespace": "default"
-    },
-    "spec": {
-        "source": {
+cat > data.json <<EOF 
+{ "metadata": { "name": "$(APPLICATION_NAME)", "namespace": "WORKER_ENV" },
+  "spec": { "source": {
             "repoURL": "https://charts.inarix.com",
             "targetRevision": "$MODEL_HELM_CHART_VERSION",
             "helm": {
                 "parameters": [
-                    {
-                        "name": "app.datadog.apiKey",
-                        "value": "$DD_API_KEY"
-                    },
-                    {
-                        "name": "app.datadog.appKey",
-                        "value": "$DD_APP_KEY"
-                    },
-                    {
-                        "name": "app.workerEnv",
-                        "value": "$WORKER_ENV"
-                    },
-                    {
-                        "name": "credentials.api.password",
-                        "value": "$INARIX_PASSWORD"
-                    },
-                    {
-                        "name": "credentials.api.username",
-                        "value": "$INARIX_USERNAME"
-                    },
-                    {
-                        "name": "credentials.aws.accessKey",
-                        "value": "$AWS_ACCESS_KEY_ID"
-                    },
-                    {
-                        "name": "credentials.aws.secretKey",
-                        "value": "$AWS_SECRET_ACCESS_KEY"
-                    },
-                    {
-                        "name": "image.imageName",
-                        "value": "$MODEL_NAME"
-                    },
-                    {
-                        "name": "image.version",
-                        "value": "$MODEL_VERSION"
-                    },
-                    {
-                        "name": "model.modelName",
-                        "value": "$NUTSHELL_MODEL_SERVING_NAME"
-                    },
-                    {
-                        "name": "model.nutshellName",
-                        "value": "$NUTSHELL_MODEL_SERVING_NAME"
-                    },
-                    {
-                        "name":"nodeSelector.name",
-                        "value": "serving-$WORKER_ENV"
-                    },
-                    {
-                        "name": "model.path",
-                        "value": "$NUTSHELL_MODEL_PATH"
-                    }
+                    { "name": "app.datadog.apiKey", "value": "$DD_API_KEY" },
+                    { "name": "app.datadog.appKey", "value": "$DD_APP_KEY" },
+                    { "name": "app.workerEnv", "value": "$WORKER_ENV" },
+                    { "name": "credentials.api.password", "value": "$INARIX_PASSWORD" },
+                    { "name": "credentials.api.username", "value": "$INARIX_USERNAME" },
+                    { "name": "credentials.aws.accessKey", "value": "$AWS_ACCESS_KEY_ID" },
+                    { "name": "credentials.aws.secretKey", "value": "$AWS_SECRET_ACCESS_KEY" },
+                    { "name": "image.imageName", "value": "$MODEL_NAME" },
+                    { "name": "image.version", "value": "$MODEL_VERSION" },
+                    { "name": "model.modelName", "value": "$NUTSHELL_MODEL_SERVING_NAME" },
+                    { "name": "model.nutshellName", "value": "$NUTSHELL_MODEL_SERVING_NAME" },
+                    { "name":"nodeSelector.name", "value": "serving-$WORKER_ENV" },
+                    { "name": "model.path", "value": "$NUTSHELL_MODEL_PATH" }
                 ]
             },
             "chart": "inarix-serving"
         },
-        "destination": {
-            "server": "https://34.91.136.161",
-            "namespace": "$WORKER_ENV"
-        },
+        "destination": { "server": "https://34.91.136.161", "namespace": "$WORKER_ENV" },
         "project": "model-serving",
         "syncPolicy": {}
     }
 }
 EOF
-cat data.json
 }
 
 function syncApplicationSpec() {
-    curl -L -X POST "${ARGOCD_ENTRYPOINT}/${APPLICATION_NAME}/sync" \
+    RESPONSE=$(curl -L -X POST "${ARGOCD_ENTRYPOINT}/${APPLICATION_NAME}/sync" \
     -s \
     -H 'Content-Type: application/json' \
-    -H "Authorization: Bearer ${ARGOCD_TOKEN}" 
+    -H "Authorization: Bearer ${ARGOCD_TOKEN}")
+    echo $CURL_RESPONSE | jq
 }
 
 function createApplicationSpec() {
     generateApplicationSpec
+    cat data.json
     CURL_RESPONSE=$(curl -L -X POST "${ARGOCD_ENTRYPOINT}" \
      -s \
      -H 'Content-Type: application/json' \
