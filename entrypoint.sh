5dbfca9d1057c30f4758847919f00727c0598b04
saisona <alexandre.saison.pro@gmail.com>
2021-06-03 16:49:58 +0200

    Update: adding debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,7 @@ function registerModel {
   if [[ ( -n $RESPONSE_CODE && $RESPONSE_CODE != 201 ) ||  $HAS_FAILED == 1 ]]
   then
     # <@USVDXF4KS> is Me (Alexandre Saison)
-    sendSlackMessage "MODEL_DEPLOYMENT" "Failed registered on Inarix API! <@USVDXF4KS> GithubAction response=$RESPONSE_CODE"  > /dev/null
+    sendSlackMessage "MODEL_DEPLOYMENT" "Failed registered on Inarix API! <@USVDXF4KS> GithubAction response=$RESPONSE_CODE and id=$HAS_FAILED"  > /dev/null
     exit 1
   else
     # <@UNT6EB562> is Artemis User
